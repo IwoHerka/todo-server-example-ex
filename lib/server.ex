790a875c0304af2d2169e659d3abf3a8ba9bd4bd
Iwo Herka <iwo.herka@makimo.pl>
2024-01-11 12:29:09 +0100

    Add simple server example

---
+++ lib/server.ex
@@ -0,0 +1,44 @@
+defmodule TodoServer do
+
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  def add_entry(server_pid, entry) do
+    send(server_pid, {:add_entry, entry})
+  end
+
+  def entries(server_pid, date) do
+    send(server_pid, {:entries, self(), date})
+
+    receive do
+      {:entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list = 
+      receive do
+        message -> process_message(message, todo_list)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message({:add_entry, entry}, todo_list) do
+    IO.inspect("Processing add_entry request")
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp process_message({:entries, client_pid, date}, todo_list) do
+    entries = TodoList.entries(todo_list, date)
+    send(client_pid, {:entries, entries})
+    todo_list
+  end
+
+  defp process_message(_, _) do
+    # error
+  end
+end
